c6fa73fb8898f422edb00f251abc43581a1a3591
waleed677 <waleedmalik677@gmail.com>
2023-09-15 03:19:21 +0500

    remove scripts

--- src/components/walletConnectModal/index.jsx
+++ src/components/walletConnectModal/index.jsx
@@ -71,7 +71,7 @@ const WalletConnectModal = (props) => {
               );
             })}
 
-          <div
+          {/* <div
             className="d-flex flex-row justify-content-start align-items-center"
             style={{ cursor: "pointer" }}
             onClick={() => handleConnectVespr()}
@@ -82,7 +82,7 @@ const WalletConnectModal = (props) => {
             <div className="p-1">
               <h2 style={{ fontSize: "24px" }}>Vespr</h2>
             </div>
-          </div>
+          </div> */}
         </div>
       </Modal.Body>
     </Modal>
